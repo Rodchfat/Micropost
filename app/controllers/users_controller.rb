8be74e72a0a50029c29619e054c707406d240666
raadchfat <raadce@gmail.com>
2016-03-13 23:52:05 +0000

    add user listing and update a show article

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController 
     
+    def index
+       @users = User.all 
+    end
+    
     def new
        @user = User.new 
     end
@@ -15,6 +19,23 @@ class UsersController < ApplicationController
            
     end
     
+    def edit
+       @user = User.find(params[:id]) 
+    end
+    
+    def update
+       @user = User.find(params[:id])
+       if @user.update(user_params)
+           flash[:notice] = "Your Prfoile is succefully updated"
+           redirect_to articles_path
+       else
+           render 'edit'
+       end
+       
+       def show 
+          @user = User.find(params[:id]) 
+       end
+    end
     private
     
     def user_params
